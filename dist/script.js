469d8cad933772681e71be45fa677097d21a5019
HI-TECHSTUDIO2024 <vladimir.yarovoy.jr@gmail.com>
2024-09-22 09:40:05 +0200

    sass

--- dist/script.js
+++
@@ -1,5 +0,0 @@
-(function () {
-  setTimeout(function () {
-    window.location = "https:volodymyr-iarovyi.github.io/Webstudio/";
-  }, 3000); /* 1000 = 1 second*/
-})();
---
+++ index.lua
@@ -0,0 +1,7 @@
+if (child.Name = "StationSensor") then
+    seat.Throttle = 40
+    seat.Throttle = 30
+    seat.Throttle = 20
+    seat.Throttle = 10
+    seat.Throttle = 0
+end
